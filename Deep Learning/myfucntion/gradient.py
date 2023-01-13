294daa82003da3720467d2b6e8470cb77f915764
tomisacat <tomisacat@live.cn>
2023-01-13 22:55:03 +0800

    0113e2

--- Deep Learning/myfucntion/gradient.py
+++ Deep Learning/myfucntion/gradient.py
@@ -1,5 +1,5 @@
 import numpy as np  
-def numerical_gradient(f,x_s):
+def mynumerical_gradient(f,x_s):
     """梯度求解
     注意，使用的是数值微分法，是基于微小值不断求导。即基于最原始的 df/dx=lim (f(x+h)-f(x-h))/h h->0 的形式
     Args:
@@ -32,3 +32,41 @@ def numerical_gradient(f,x_s):
         #print("f1-f2/h计算结果",d_x[i])
         
     return d_x
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 还原值
+        it.iternext()   
+        
+    return grad
+
+def gradient_descent(f,init_x,lr=0.01,step_num=100):
+    """梯度下降法
+
+    Args:
+        f (_type_): 误差函数
+        init_x (_type_): 权重初始值
+        lr (float, optional): 学习率.
+        step_num (int, optional): 循环次数. Defaults to 100.
+
+    Returns:
+        _type_: 更新后的新权重，实现误差函数达到极小值
+    """
+    x=init_x
+    for i in range(step_num):
+        grad=numerical_gradient(f,x)
+        x=x-lr*grad
+    return x 
+ 
