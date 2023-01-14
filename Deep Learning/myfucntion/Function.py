42c6c60c2a1ceb294ba1b94ef5c3d25db6133608
tomisacat <tomisacat@live.cn>
2023-01-14 15:44:20 +0800

    0114a

--- Deep Learning/myfucntion/Function.py
+++ Deep Learning/myfucntion/Function.py
@@ -34,7 +34,22 @@ def cross_entropy_e(y,y_p):
     n=y.shape[0]
     e=-np.sum(y*np.log(y_p+delta))/n
     return e
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 监督数据是one-hot-vector的情况下，转换为正确解标签的索引
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
+
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
 def mean_sqared_e(y,y_p):
     """均方误差函数
 
